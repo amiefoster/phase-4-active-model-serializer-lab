50c5b738be80fd40b3abb4f9ea39c243aa2eb39c
Amie Foster <amie.n.foster@gmail.com>
2021-11-04 22:33:24 -0600

    16/20

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,8 +1,6 @@
 class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :content
 
-  has_many :tags
-
   #write out a short_content method to show first 40 characters and then render it in the controller EXPLICITLY
   #need to include tags
 end
